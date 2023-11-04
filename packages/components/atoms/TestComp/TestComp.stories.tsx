f2f5c8fa56dcab4f553a339092c3a161f208bd16
callumhemming <callum.hemming@frasers.group>
2023-11-04 18:04:08 +0000

    chore: set up storybook

---
+++ packages/components/atoms/TestComp/TestComp.stories.tsx
@@ -0,0 +1,8 @@
+import { TestComp as TestCompComp } from "./TestComp";
+
+export const TestComp = () => <TestCompComp />;
+
+export default {
+  component: TestComp,
+  title: "atoms/TestComp",
+};
---
+++ packages/components/atoms/TestComp/TestComp.tsx
@@ -0,0 +1,3 @@
+import classes from "./TestComp.module.css";
+
+export const TestComp = () => <div className={classes.test}>Test</div>;
